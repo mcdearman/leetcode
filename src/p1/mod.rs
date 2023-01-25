c122bd449acd593390faf375ac77663fe39a8225
mcdearman <mcdearmanchris@gmail.com>
2023-01-25 05:12:33 -0600

    add two_sum tests

--- src/p1/mod.rs
+++ src/p1/mod.rs
@@ -13,9 +13,31 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 }
 
 #[test]
-fn test_two_sum() {
+fn test1_two_sum() {
     assert_eq!(
         two_sum(vec![2, 7, 11, 15], 9)
+            .iter()
+            .copied()
+            .collect::<HashSet<i32>>(),
+        vec![1, 0].iter().copied().collect::<HashSet<i32>>()
+    );
+}
+
+#[test]
+fn test2_two_sum() {
+    assert_eq!(
+        two_sum(vec![3, 2, 4], 6)
+            .iter()
+            .copied()
+            .collect::<HashSet<i32>>(),
+        vec![2, 1].iter().copied().collect::<HashSet<i32>>()
+    );
+}
+
+#[test]
+fn test3_two_sum() {
+    assert_eq!(
+        two_sum(vec![3, 3], 6)
             .iter()
             .copied()
             .collect::<HashSet<i32>>(),
